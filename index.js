2b13b925efef6c839a38af948d7fc2915662a1de
Alexander Musyoki <amusyoki.gm@gmail.com>
2023-03-14 11:42:05 +0300

    Added Procfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = port.env.PORT||3000;
 const bodyParser = require('body-parser');
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
 const jsonParser = express.json();
@@ -63,4 +63,4 @@ app.post('/calculate-bmi', urlEncodedParser, jsonParser, (request, response) => 
 });
 
 app.listen(port);
-console.log('Server listening on port 3000');
+console.log('Server listening on port ${port}');
